406760192ed66550581cf57b41b4439b9db27e17
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-11-11 15:59:53 +0100

    fix(settings): merge local and travis in one settings file

---
+++ config/settings/ci.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import find_dotenv
+from dotenv import load_dotenv
+
+from .base import *  # noqa: F401, F403
+
+
+ALLOWED_HOSTS = ["*"]
+
+
+load_dotenv(find_dotenv(filename=".env-local"))
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="foo-key")
+DEBUG = True if os.getenv("DEBUG") == "True" else False
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_NAME", default="postgres"),
+        "USER": os.getenv("POSTGRES_USER", default="postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default=""),
+        "HOST": os.getenv("POSTGRES_HOST", default=""),
+        "PORT": os.getenv("POSTGRES_PORT", default=""),
+    }
+}
