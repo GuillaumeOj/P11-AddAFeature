04fdeb60e58691f45567159dfbf19a9d78dbec69
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-11-21 10:28:49 +0100

    feat(email): add settings for email backend

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -8,8 +8,6 @@ ALLOWED_HOSTS = ["*"]
 
 load_dotenv(find_dotenv(filename=".env-local"))
 
-SENDGRID_APY_KEY = os.getenv("SENDGRID_APY_KEY")
-
 SECRET_KEY = os.getenv("SECRET_KEY", default="foo-key")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
@@ -24,3 +22,11 @@ DATABASES = {
         "PORT": os.getenv("POSTGRES_PORT", default=""),
     }
 }
+
+# Settings for email client
+EMAIL_HOST = os.getenv("EMAIL_HOST", default="localhost")
+EMAIL_PORT = os.getenv("EMAIL_PORT", default=424242)
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", default="foo")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", default="bar")
+EMAIL_USE_SSL = True if os.getenv("EMAIL_USE_SSL") else False
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
